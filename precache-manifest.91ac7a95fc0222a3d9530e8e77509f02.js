d6070a3d1f4940d1826563c781443a77daaa2beb
Dao Van Thang <daothang.net@gmail.com>
2020-07-19 22:46:02 +0700

    Updates

---
+++ precache-manifest.91ac7a95fc0222a3d9530e8e77509f02.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3713e530a636f0b1656d415dbba04d48",
+    "url": "/webapp/index.html"
+  },
+  {
+    "revision": "6a5e7817767d99d5cd60",
+    "url": "/webapp/static/css/2.73353908.chunk.css"
+  },
+  {
+    "revision": "af50e3ce87b6c770ad31",
+    "url": "/webapp/static/css/main.e2164058.chunk.css"
+  },
+  {
+    "revision": "6a5e7817767d99d5cd60",
+    "url": "/webapp/static/js/2.1d7612ed.chunk.js"
+  },
+  {
+    "revision": "8a0f87d4ff9462a28d37926fe68f0ebe",
+    "url": "/webapp/static/js/2.1d7612ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af50e3ce87b6c770ad31",
+    "url": "/webapp/static/js/main.253f487f.chunk.js"
+  },
+  {
+    "revision": "f14210e82fe215c326f9",
+    "url": "/webapp/static/js/runtime-main.58817eac.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/webapp/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/webapp/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/webapp/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/webapp/static/media/slick.f97e3bbf.svg"
+  }
+]);
